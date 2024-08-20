21fe71cbe137f7a6e76b75473429e24876a86356
blindjedi <matt6js@gmail.com>
2024-08-19 18:31:17 -0600

    update ui for list display

--- src/app/quotes/components/QuoteList.js
+++ src/app/quotes/components/QuoteList.js
@@ -1,13 +1,19 @@
 'use client';
 
 export default function QuoteList({ quotes }) {
+  const backgroundClasses = ['bg-tile-1', 'bg-tile-2', 'bg-tile-3'];
+
   return (
-    <ul className="mt-6 space-y-2">
-      {quotes.map((quote) => (
-        <li key={quote.id} className="bg-gray-100 p-4 rounded shadow text-black">
-          {quote.text}
-        </li>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-6">
+      {quotes.map((quote, index) => (
+        <div
+          key={quote.id}
+          className={`quote-tile ${backgroundClasses[index % backgroundClasses.length]} p-4 rounded shadow text-black`}
+          style={{ whiteSpace: 'pre-line' }}
+        >
+          {quote.text.replace(/, /g, ',\n').replace(/\. /g, '.\n')}
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
